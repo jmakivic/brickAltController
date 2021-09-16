b3be75a3c08075bf3baf917bc7b7ad15a9de4dd9
jmakivic <jmakivic@gmail.com>
2021-09-16 17:38:41 +0200

    Adding the latest version of my game

--- webserver.js
+++ webserver.js
@@ -2,7 +2,7 @@ var http=require('http');
 var fs = require('fs');
 var express = require('express');
 
-
+//set up the server to listen to port 8080
 var app = express();
 app.use(express.static('public'));
 app.set('port','8080');
@@ -16,52 +16,32 @@ server.on('listening', ()=>{
 var soc_io = require('socket.io')(server, {
     allowEIO3:true,
     "upgrades": ["websocket"],
-    "pingInterval": 60000,
+    "pingInterval": 30000,
     "pingTimeout": 1000
     
 });
 
+//this variable will be used to store the speed information coming from the client
+let new_speed =0;
 
-
-
-// reply to request with "Hello World!"
-/*app.get('/', function (req, res) {
-  res.render('index.html');
-});*/
-
-
-
-
-/*transports: ['websocket'],
-pingInterval: 1000 * 60 * 5,
-pingTimeout: 1000 * 60 * 3*/
+//use this to access the raspi-io library
 var Raspi = require("raspi-io").RaspiIO;
 
+//use this to access the johnny-five library
 var five = require("johnny-five");
+
+//use the acceleremoter library from johnny five
 const { Accelerometer, Board } = require("johnny-five");
+
+//establish what kind of board you are using, in this case it is a Raspberry Pi
 var board = new five.Board({
     io: new Raspi()
 });
 
+//calling the server and making it listen to your device's local ip address on port 8080
+server.listen(8080,'your.devices.local.ip')
 
-
-
-/*function handler(req, res){
-    fs.readFile('./public/index.html', function(err, data){
-
-        if(err){
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end('404 Not Found');
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-
-}*/
-
-server.listen(8080,'192.168.1.8')
-
+//the websocket is accepting data coming in from the client
 soc_io.sockets.on('connection', function(socket){
 
     console.log('Client connected: ' + socket.id)
@@ -71,31 +51,35 @@ soc_io.sockets.on('connection', function(socket){
     
     console.log(socket.connected);
     
-    console.log("Board: ");
+    //the socket is taking in speed data which is being sent from the client
+    socket.on('speed',function(speed){
+        //store the speed data in the new_speed variable
+        new_speed = speed;
+    });
     
 });
 
-
-
-
+//establishing the connection to the Raspberry Pi and its GPIO pins
 board.on("ready", ()=>{
+    //declare the new acceleremoter
     const accelerometer = new Accelerometer({
         controller: "LIS3DH",
     });
 
-    console.log("defined piezo");
-
+    //assign GPIO pins to each piezo
+    //This controller uses vibration motors instead of piezos. Treating them as piezos allows them to have more nuanced vibration patterns
     var piezo = new five.Piezo("P1-13");
+    var piezo2 = new five.Piezo("P1-40");
 
     // Injects the piezo into the repl
     board.repl.inject({
-      piezo: piezo
+      piezo: piezo,
+      piezo2:piezo2
     });
-    //If placed here it only plays once
+    //If placed here, the piezo only plays once it only plays once
     piezo.play({
         // song is composed by an array of pairs of notes and beats
-        // The first argument is the note (null means "no note")
-        // The second argument is the length of time (beat) of the note (or non-note)
+     
         song: [
           ["C4", 1 / 4],
           ["D4", 1 / 4],
@@ -119,10 +103,13 @@ board.on("ready", ()=>{
         tempo: 100
       });
     // Plays a song
-       
+
+    //accelerometer.on is an infinite loop. If any code is placed after this loop, it will not be accessed because this loop never ends.
+    //This is why I chose to play the piezos inside of this loop, as well    
     accelerometer.on("change", ()=>{
         const {acceleration, inclination, orientation, pitch, roll, x,y,z} =accelerometer;
-    
+        let basic_coors = [x,y,z];
+        soc_io.sockets.emit('coors', basic_coors);
         soc_io.sockets.emit('accel', acceleration);
         soc_io.sockets.emit('incl', inclination);
         soc_io.sockets.emit('orient', orientation);
@@ -133,34 +120,59 @@ board.on("ready", ()=>{
         soc_io.sockets.emit('zPos', z);
 
         //placing it over here makes it play multiple times. But long songs do not get played fully for
-        //some reason. Not entirely sure why. But I am okay with this I think. 
-        piezo.play({
-            // song is composed by an array of pairs of notes and beats
-            // The first argument is the note (null means "no note")
-            // The second argument is the length of time (beat) of the note (or non-note)
-            song: [
-              
-              ["G4", 1 ],
-             
-            ],
-            tempo: 100
-          });
-        // Plays a song
-        
-    
-    
+        //some reason. Not entirely sure why. But I am okay with this because it still serves the purposes of my game. 
 
+        //the new_speed variable which stores the speed data coming in from the client determines what kind of "note" the vibration motors will play
+        if(new_speed >0.08){
+            piezo.play({
+                // song is composed by an array of pairs of notes and beats
+      
+                song: [
+                  
+                  ["G4", 1 ],
+                 
+                ],
+                tempo: 100
+              });
+            piezo2.play({
+                // song is composed by an array of pairs of notes and beats
+
+                song: [
+                  
+                  ["F4", 2 ],
+                 
+                ],
+                tempo: 100
+              });
+
+        }
+
+        if(new_speed <0.06){
+            piezo.play({
+                // song is composed by an array of pairs of notes and beats
+
+                song: [
+                  
+                  ["A4", 1 ],
+                 
+                ],
+                tempo: 100
+              });
+            piezo2.play({
+                // song is composed by an array of pairs of notes and beats
+      
+                song: [
+                  
+                  ["C4", 1/4 ],
+                 
+                ],
+                tempo: 100
+              });
+
+        }
+        
     });
     
- 
-    
-
-  
-
-    
-
-    //Set up stuff for piezo here. Just have it running, no need to have it react to anything just yet.
-    //There is no reason for it to be the accelerometer function 
               
     board.on("exit", () =>{
             accelerometer.off();
